b2de352c8affbb929af44570ca129d78317cabf9
Victor Alves Lessa Araujo <jegrash55@tutanota.com>
2024-01-14 19:19:51 -0300

    c

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,11 @@ import Fastify from 'fastify';
 import cors from '@fastify/cors'
 import { routes } from './routes';
 import { request } from 'http';
+import { hostname } from 'os';
 
 const app = Fastify({ logger: true })
 const PORT = process.env.PORT || 10000;
+const HOST = process.env.HOST || '0.0.0.0';
 app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message })
 })
@@ -14,7 +16,7 @@ const start = async() =>{
     await app.register(cors)
     await app.register(routes)
     try{
-        await app.listen(PORT, () => {
+        await app.listen(PORT, HOST, () => {
             console.log(`server started on port ${PORT}`);
           });
     }
